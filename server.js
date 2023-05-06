52359e4e7b1a66802aa3dfe294a574ee65ac9b52
183176saad <68743530+183176saad@users.noreply.github.com>
2023-05-06 11:08:18 +0500

    asd

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const http = require("http");
+const http = require('http');
 
-const myVariable = "4d0d9d9d4b7cab8986aa90db0c7ed07964a8acf5405bbf746aebd7f1c80e87b6"|| process.env.PRIVATE_KEY;
+const myVariable = process.env.PRIVATE_KEY || 'default-value';
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write(myVariable);
-    res.end();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(myVariable);
 });
 
-server.listen(3000, () => {
-    console.log("Server running on 3000");
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
