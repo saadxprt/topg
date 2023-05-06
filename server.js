0661fd458ee5e5bcb7a7636cbac400778a3ec147
183176saad <68743530+183176saad@users.noreply.github.com>
2023-05-06 11:10:47 +0500

    asd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const myVariable = process.env.PRIVATE_KEY || 'default-value';
+const myVariable = process.env.PRIVATE_KEY || '4d0d9d9d4b7cab8986aa90db0c7ed07964a8acf5405bbf746aebd7f1c80e87b6';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
