006a94f24caf52f5481235285b067a35c6112a14
183176saad <68743530+183176saad@users.noreply.github.com>
2023-05-06 10:55:05 +0500

    Update myvariable.js

--- pages/api/myvariable.js
+++ pages/api/myvariable.js
@@ -2,3 +2,7 @@ export default function handler(req, res) {
   const myVariable = process.env.PRIVATE_KEY || "4d0d9d9d4b7cab8986aa90db0c7ed07964a8acf5405bbf746aebd7f1c80e87b6";
   res.status(200).send(myVariable);
 }
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
